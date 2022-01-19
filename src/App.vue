20335d2ead45e125358a3f8594ef273f59985390
Marian Adamus <cmanar0@gmail.com>
2022-01-19 13:55:52 +0100

    Create Vue 3 project using CLI

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <Timeline />
+  </div>
+</template>
+
+<script>
+import Timeline from './components/Timeline.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Timeline
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Timeline.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <div class="time-line">
+      <div class="line" id="line">
+        <div><p>sadasdads</p></div>
+        <div><p>sadasdads</p></div>
+        <div><p>sadasdads</p></div>
+        <div><p>sadasdads</p></div>
+        <div><p>sadasdads</p></div>
+        <div><p>sadasdads</p></div>
+        <div><p>sadasdads</p></div>
+        <div><p>sadasdads</p></div>
+        <div><p>sadasdads</p></div>
+        <div><p>sadasdads</p></div>
+        <div><p>sadasdads</p></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#line::before {
+  font-family: 'Font Awesome 5 Free';
+  font-weight: 900;
+  content: '\f007';
+  position: relative;
+  left: -50px;
+  top: 0;
+}
+#line::after {
+  font-family: 'Font Awesome 5 Free';
+  font-weight: 900;
+  content: '\f015';
+}
+.time-line .bottom-p {
+  position: relative;
+  left: -9px;
+  top: calc(auto - 20px);
+}
+.point {
+  height: 15px;
+  width: 15px;
+  background-color: rgba(13, 81, 132, 255);
+  border-radius: 50%;
+}
+.time-line {
+  display: flex;
+  flex-direction: column;
+}
+.line {
+  border-left: 4px solid rgba(13, 81, 132, 255);
+  height: calc(auto - 40px);
+  position: absolute;
+  left: 50%;
+  top: 20px;
+  margin-left: -3px;
+}
+</style>
